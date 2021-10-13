7458e89f500e59f90cbf709c33c3349786bb3adf
Jakob Unnebäck <jakob@folkdatorn.se>
2021-10-13 17:00:22 +0200

    Updated design

--- lab4/keylogger/client/src/components/Logs.js
+++ lab4/keylogger/client/src/components/Logs.js
@@ -1,12 +1,38 @@
 import React, { useEffect, useState } from 'react'
 import styled from 'styled-components'
-
+// max-width: 800px;
 const Container = styled.div`
-    max-width: 800px;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    flex: 1;
+`
+
+const List = styled.div`
+    overflow: scroll;
+    max-height: 50vh;
 `
 
 const Title = styled.h1`
     color: #fff;
+`
+
+const ListContainer = styled.div`
+    position: relative;
+    max-width: 500px;
+    width: 100%;
+    max-height: 50vh;
+`
+
+const Fade = styled.div`
+    position: absolute;
+    left: 0;
+    right: 0;
+    bottom: -1px;
+    height: 100px;
+    background: rgb(229,122,68);
+    background: linear-gradient(180deg, rgba(229,122,68,0) 0%, rgba(229,122,68,1) 100%);
 `
 
 const Logs = ({ }) => {
@@ -23,17 +49,27 @@ const Logs = ({ }) => {
         fetchData()
     }, [])
 
+    if (logs.length == 0) {
+        return (<Title>Loading...</Title>)
+    }
+
     return (
         <Container>
-            <Title>Secure keylogger</Title>
-            <div>
-                {logs.map(log => <Log>{log}</Log>)}
-            </div>
+            <Title>Secure* keylogger</Title>
+
+            <ListContainer>
+                <List>
+                    {logs.map(log => <Log>{log}</Log>)}
+                </List>
+                <Fade></Fade>
+            </ListContainer>
         </Container>
     )
 }
 
 const LogContainer = styled.div`
+    display: flex;
+    flex: 1;
     background-color: #fff;
     border-radius: 10px;
     padding: 20px;
